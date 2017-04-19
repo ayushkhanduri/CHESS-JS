20499d2c00e2753c97f121a4d5d5f17cd9ffa39e
ayushkhanduri khanduri <ayushkhanduri@gmail.com>
2017-04-20 00:09:30 +0530

    Chess board ready

--- main.js
+++ main.js
@@ -19,44 +19,61 @@
   //this is a function of the framework inside which the main game object will be created and main where game loop will run
   var MainGameFramework = (function(){
       //all private variables
-      var self = this, canvas; // bind variables to _self in order to add properties to the objects;
-      var CONST_WIDTH,CONST_HEIGHT,LEFT_OFFSET,TOP_OFFSET,STARTING_POINT_X,STARTING_POINT_Y; // all capital letters are all constants and are private , if you want to access those elements make getters
+      var _self
+      var CONST_WIDTH,CONST_HEIGHT,LEFT_OFFSET,TOP_OFFSET,STARTING_POINT_X,STARTING_POINT_Y,NOS; // all capital letters are all constants and are private , if you want to access those elements make getters
       var SCREEN_WIDTH,SCREEN_HEIGHT;// assuming that the window won't resize while playing the game
+      var DIFF_COLORS =[];
       // main constructor function to be returned , used for setting up configuration and object
       function MainGame(canvas){
         //initialize all the variables which would add to the property of the object
+        _self= this; // so that we can bind other function's variables 
         initializePrivateConstants();
-        calculateXY();
-        self.canvasContext = canvas.getContext("2d");
         $('#mainGame').style.left = LEFT_OFFSET;
         $('#mainGame').style.top = TOP_OFFSET;
         canvas.width= CONST_WIDTH;
         canvas.height = CONST_HEIGHT;
-        console.log(self.canvasContext);
+        _self.canvasContext = canvas.getContext("2d");
+        //calculateXY();
+        drawingBoard();
+        //console.log(_self);
       }
+      
+      // this function is no longer needed
+      // function calculateXY(){
+      //   var percent = LEFT_OFFSET.substring(0,LEFT_OFFSET.length-1);
+      //   STARTING_POINT_X = ((parseInt(percent)/100) * SCREEN_WIDTH).toFixed(2);
+      //   percent = TOP_OFFSET.substring(0,TOP_OFFSET.length-1);
+      //   STARTING_POINT_Y = ((parseInt(percent)/100) * SCREEN_HEIGHT).toFixed(2);
+      //   console.log(STARTING_POINT_X +  " : " + STARTING_POINT_Y);
+      //   //STARTING_POINT_Y = (parseInt(per))
+      //   drawingBoard();
+      // }
 
-      function calculateXY(){
-        var percent = LEFT_OFFSET.substring(0,LEFT_OFFSET.length-1);
-        STARTING_POINT_X = ((parseInt(percent)/100) * SCREEN_WIDTH).toFixed(2);
-        drawVerticalLines();
-        percent = TOP_OFFSET.substring(0,TOP_OFFSET.length-1);
-        STARTING_POINT_Y = ((parseInt(percent)/100) * SCREEN_HEIGHT).toFixed(2);
-        console.log(STARTING_POINT_X +  " : " + STARTING_POINT_Y);
-        //STARTING_POINT_Y = (parseInt(per))
-      }
-
-      function drawVerticalLines(){
-
+      function drawingBoard(){
+        var eachSideLength = CONST_WIDTH/8;
+        var xCoor =0, yCoor=0;
+        //_self.canvasContext.fillStyle = "#000";
+        //_self.canvasContext.fillRect(20,20,100,100);
+        for(var i=0;i<NOS;i++){
+          yCoor = (eachSideLength * i);
+          for(var j=0; j<NOS;j++){
+            xCoor = (eachSideLength * j);
+            _self.canvasContext.fillStyle= DIFF_COLORS[(i+j)%2];
+            _self.canvasContext.fillRect(xCoor,yCoor,eachSideLength,eachSideLength);
+          }
+        }
       }
 
       //this is a private function , not accessable from the object 
       function initializePrivateConstants(){
-        CONST_WIDTH = 400;
-        CONST_HEIGHT = 400;
+        CONST_WIDTH = (33/100) * window.innerWidth;
+        CONST_HEIGHT = CONST_WIDTH;
+        NOS = 8; // number of sides 
         LEFT_OFFSET="33%";
         TOP_OFFSET="15%";
         SCREEN_WIDTH = window.innerWidth;
         SCREEN_HEIGHT = window.innerHeight;
+        DIFF_COLORS = ["#FFF","#000"];
 
       }
       //  custom $ implementation of jquery ,
